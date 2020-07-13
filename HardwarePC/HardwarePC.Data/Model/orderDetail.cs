c78cb9c66d1a2eae482eadfc6b62afb6f5b256a2
rborquez <rborquez@cecaitra.org.ar>
2020-07-13 19:26:20 -0300

    Entrega TP FINAL - LPPA

---
+++ HardwarePC/HardwarePC.Data/Model/orderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwarePC.Data.Model
+{
+    public class orderDetail : IdentityBase
+    {
+        public int orderId;
+        public int productId;
+        public float price;
+        public int quantity;
+        public virtual Order order { get; set; }
+    }
+}
